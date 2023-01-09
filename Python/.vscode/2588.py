88ee9b4d6663a6538388731c05218cb4f01e1e31
oh-seunghun <osh6255@gmail.com>
2023-01-09 20:56:59 +0900

    commit

--- Python/.vscode/2588.py
+++ Python/.vscode/2588.py
@@ -1,4 +1,10 @@
 a = input()
-a = int(a)
 b = input()
-b = int(b)
+a = int(a)
+b = list(b)
+
+print(a * int(b[2]))
+print(a * int(b[1]))
+print(a * int(b[0]))
+c = int(b[0]) * 100 + int(b[1]) * 10 + int(b[2])
+print(a*c)
---
+++ Python/.vscode/3046.py
@@ -0,0 +1,3 @@
+a, b = input().split()
+c = (int(b)*2) - int(a)
+print(c)
