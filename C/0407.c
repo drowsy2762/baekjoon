1030c6e806f2bb673c81854299fbf288944ec493
drowsy2762 <drowsy2762@gmail.com>
2023-04-07 17:17:04 +0900

    utf 바꿨더니 지랄

--- C/0407.c
+++ C/0407.c
@@ -4,7 +4,7 @@
 /*int main(){
     int i = 0;
     int n = 0;
-    printf("êµ¬êµ¬ë‹¨ì˜ ì‹œì‘ì ì„ ì…ë ¥í•´ì£¼ì„¸ìš”: ");
+    printf("êµ¬êµ¬?‹¨?˜ ?‹œ?‘? ?„ ?…? ¥?•´ì£¼ì„¸?š”: ");
     scanf("%d",&n);
     while (i<9){
         i++;
@@ -15,40 +15,40 @@
 /*int main(){
     int score = 0;
     double sum = 0, average = 0;
-    printf("ì ìˆ˜ì˜ ì…ë ¥ì„ ì¢…ë£Œí•˜ë ¤ë©´ ìŒìˆ˜ë¥¼ ì…ë ¥í•´ì£¼ì„¸ìš”.\n");
+    printf("? ?ˆ˜?˜ ?…? ¥?„ ì¢…ë£Œ?•˜? ¤ë©? ?Œ?ˆ˜ë¥? ?…? ¥?•´ì£¼ì„¸?š”.\n");
     while(score>=0){
         average++;
-        printf("ì ìˆ˜ë¥¼ ì…ë ¥í•´ ì£¼ì„¸ìš”: ");
+        printf("? ?ˆ˜ë¥? ?…? ¥?•´ ì£¼ì„¸?š”: ");
         scanf("%d",&score);
         sum += score;
     }
-    printf("í‰ê·  ì ìˆ˜ëŠ” %.1lfì…ë‹ˆë‹¤.",sum/average);
+    printf("?‰ê·? ? ?ˆ˜?Š” %.1lf?…?‹ˆ?‹¤.",sum/average);
 }*/
 
 /*int main(){ 
     int sum = 0;
     int num ;
     do{
-        printf("ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
+        printf("? •?ˆ˜ë¥? ?…? ¥?•˜?„¸?š”: ");
         scanf("%d",&num);
         sum += num;
     }while(num != 0);
-    printf("ì…ë ¥í•œ ì •ìˆ˜ë“¤ì˜ í•©: %d\n",sum);
+    printf("?…? ¥?•œ ? •?ˆ˜?“¤?˜ ?•©: %d\n",sum);
     return 0;
 }*/
 
 /*int main(){
     int i,sum;
-    printf("1ë¶€í„° 10ê¹Œì§€ì˜ í•©ì„ êµ¬í•©ë‹ˆë‹¤.\n");
+    printf("1ë¶??„° 10ê¹Œì???˜ ?•©?„ êµ¬í•©?‹ˆ?‹¤.\n");
     for(i=1;i<=10;i++){
         sum += i;
     }
-    printf("1ë¶€í„° 10ê¹Œì§€ì˜ í•©ì€ %dì…ë‹ˆë‹¤.\n",sum);
+    printf("1ë¶??„° 10ê¹Œì???˜ ?•©??? %d?…?‹ˆ?‹¤.\n",sum);
 }*/
 
 /* int main(){
     int n,factorial;
-    printf("ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
+    printf("? •?ˆ˜ë¥? ?…? ¥?•˜?„¸?š”: ");
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
         factorial *= i;
@@ -56,18 +56,32 @@
     printf("%d! = %d\n",n,factorial);
 } */
 
-int main(){
+/*int main(){
     int n,sum=0;
     char c;
     while(1){
-        printf("ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
+        printf("? •?ˆ˜ë¥? ?…? ¥?•˜?„¸?š”: ");
         scanf("%d",&n);        
-        printf("ê³„ì†í•˜ì‹œê² ìŠµë‹ˆê¹Œ?(y/n): ");
+        printf("ê³„ì†?•˜?‹œê² ìŠµ?‹ˆê¹??(y/n): ");
         scanf("%c",&c);
         sum += n;
         if(c == 'n'){
             break;
         }
     }
-    printf("í•©ê³„: %d\n",sum);
+    printf("?•©ê³?: %d\n",sum);
+} */
+
+int main(){
+    char ch;
+    char letter;
+    while(1){
+        printf("ë¬¸ìë¥? ?…? ¥?•˜?„¸?š”: (?†Œë¬¸ìë¥? ?…? ¥?•´ì£¼ì„¸?š”, ì¢…ë£Œ?•˜? ¤ë©? Që¥? ?…? ¥?•´ì£¼ì„¸?š”))");
+        scanf("%c",&letter);
+        ch = letter-32;
+        if(letter == 'Q')
+            break;
+        if ( letter <'a' || letter > 'z')
+            printf("???ë¬¸ìë¡? ë³??™˜: %c",ch);
+    }
 }
