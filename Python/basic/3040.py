4787598062e0c3015b9e6b69a6aa8ca65bb5a1ac
drowsy2762 <drowsy2762@gmail.com>
2023-04-14 11:29:02 +0900

    root change

---
+++ Python/basic/3040.py
@@ -0,0 +1,17 @@
+# 2309 하고 동일
+a = []
+break_point = 0
+for i in range(9) :
+    a.append(int(input()))
+for i in range(9) :
+    for j in range(9) :
+        if(sum(a)-a[i]-a[j] == 100 and i != j):
+            del a[j]
+            del a[i]
+            break_point += 1
+            break;
+    if(break_point == 1):
+        break;
+a.sort()
+for i in range(7):
+    print(a[i])
