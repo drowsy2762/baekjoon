3372d243366fb3489e3ae09e1ebdde6a9e976f55
drowsy2762 <drowsy2762@gmail.com>
2023-04-20 20:20:39 +0900

    test

---
+++ Python/samsung_test/13460.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+board = [[0] * n] * m
+for i in range(m):
+    board[i] = list(input())
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == "R":
+            red = [i, j]
+        elif board[i][j] == "B":
+            blue = [i, j]
+        elif board[i][j] == "O":
+            hole = [i, j]
+    if red == hole:
+        break
+print(board)
+
+# red - > hole blue !-> hole
+#
