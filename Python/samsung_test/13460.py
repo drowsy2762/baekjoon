4c6f70b90161f815bc77228a1700b820ca564887
drowsy2762 <drowsy2762@gmail.com>
2023-04-27 15:00:03 +0900

    what

--- Python/samsung_test/13460.py
+++ Python/samsung_test/13460.py
@@ -1,8 +1,13 @@
+def move_red(x, y):
+    return 0
+
+
 n, m = map(int, input().split())
 board = [[0] * n] * m
+cnt = 0
 for i in range(m):
     board[i] = list(input())
-for i in range(n):
+for i in range(11):
     for j in range(m):
         if board[i][j] == "R":
             red = [i, j]
@@ -10,6 +15,14 @@ for i in range(n):
             blue = [i, j]
         elif board[i][j] == "O":
             hole = [i, j]
-    if red == hole:
+"""
+if red == hole:
         break
+    if(cnt > 10):
+        break
+if cnt > 10:
+    print(-1)
+else:
+    print(cnt)
+"""
 print(board)
