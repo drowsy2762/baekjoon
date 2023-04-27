47ba7a962be3a591954ea5dbfefe9028851791ee
drowsy2762 <drowsy2762@gmail.com>
2023-04-27 21:38:38 +0900

    moves

--- Python/basic/101-150/1157.py
+++ Python/basic/101-150/1157.py
@@ -1,1 +1,11 @@
-s = input()
+s = input().lower()
+s_l = list(set(s))
+cnt = []
+
+for i in s_l:
+    cnt.append(s.count(i))
+
+if cnt.count(max(cnt)) > 1:
+    print("?")
+else:
+    print(s_l[cnt.index(max(cnt))].upper())
--- Python/basic/4458.py
+++
@@ -1,4 +0,0 @@
-n = int(input())
-for i in range(n):
-    s = input()
-    print(s[0].upper() + s[1:])
