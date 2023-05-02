5d96833fd2d7269974234981ed5ac511eb539470
drowsy2762 <drowsy2762@gmail.com>
2023-05-03 00:18:36 +0900

    230502

--- Python/basic/151~200/5596.py
+++ Python/basic/151~200/5596.py
@@ -1,8 +1,13 @@
 # 시험점수 : https://www.acmicpc.net/problem/5596 (python3)
 
-min_score = input()
-man_score = input()
-s1, s2 = 0
+min_score = input().split()
+man_score = input().split()
+s1 = 0
+s2 = 0
 for i in range(4):
     s1 += int(min_score[i])
     s2 += int(man_score[i])
+if s1 > s2:
+    print(s1)
+else:
+    print(s2)
