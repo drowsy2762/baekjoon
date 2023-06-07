faeb8a77cd2d626926ce43cd62a997bc0847766f
drowsy2762 <drowsy2762@gmail.com>
2023-06-07 18:17:30 +0900

    complete code but time error

--- Python/shortTime/3197.py
+++ Python/shortTime/3197.py
@@ -1,48 +1,72 @@
 # https://www.acmicpc.net/problem/3197 : 백조의 호수 (python3)
 from collections import deque
 
-# 1. 물과 백조의 위치를 각각 저장
-# 2. 물과 접촉한 모든 빙판공간을 녹임
-# 3. 백조가 서로 접촉할 수 있는지 확인
-# 4. 반복
-# 5. 백조가 접촉할 수 있게되면 프로그램을 종료하고 걸리는 날을 출력
-
-# 너비우선탐색 알고리즘
-# 1. 탐색 시작 노드를 큐에 삽입하고 방문처리
-# 2. 큐에서 노드를 꺼내 해당 노드의 인접노드 중 방문하지 않은 노드를 모두 큐에 삽입하고 방문처리
-# 3. 2번의 과정을 더 이상 수행할 수 없을 때까지 반복
+# L 은 오리 .은 물 X 는 빙판
 
 
-def bfs(x, y, visited):
+# 빙판을 녹이는 과정
+def bfs(x, y):
+    queue = deque()
+    melt = []
+    queue.append((x, y))
+    visited[x][y] = True
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    while queue:
+        cx, cy = queue.popleft()
+        for i in range(4):
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0 > nx or nx >= r or 0 > ny or ny >= c:
+                continue
+            if lake[cx][cy] == "." and lake[nx][ny] == "X":
+                melt.append((nx, ny))
+            if not visited[nx][ny]:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+    return melt
+
+
+def melt(melt):
+    for i in melt:
+        lake[i[0]][i[1]] = "."
+
+
+# 오리가 만나는지 확인
+def swan(x, y):
     queue = deque()
     queue.append((x, y))
     visited[x][y] = True
+    swan = []
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
     while queue:
-        x, y = queue.popleft()
+        cx, cy = queue.popleft()
         for i in range(4):
-            nx, ny = dx, dy
-            if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            nx = cx + dx[i]
+            ny = cy + dy[i]
+            if 0 > nx or nx >= r or 0 > ny or ny >= c:
                 continue
-            if not visited[nx][ny]:
-                visited[nx][ny] = True
+            if lake[nx][ny] == "L" and not visited[nx][ny]:
+                swan.append((nx, ny))
+            if not visited[nx][ny] and lake[nx][ny] == ".":
                 queue.append((nx, ny))
+            visited[nx][ny] = True
+    return swan
 
 
-# 호수 크기를 입력받고 물과 백조, 얼음의 위치를 받음
+# 호수의 크기를 입력받음
 r, c = map(int, input().split())
 lake = [list(input()) for _ in range(r)]
-
-# 물과 백조의 위치를 각각 저장
-water = []
-swan = []
-for i in range(r):
-    for j in range(c):
-        if lake[i][j] == ".":
-            water.append((i, j))
-        elif lake[i][j] == "L":
-            swan.append((i, j))
-
-# 물과 접촉한 모든 빙판공간을 녹임
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-visited = [[False] * c for _ in range(r)]
+day = 0
+# 호수의 크기만큼 방문 여부를 저장할 리스트 생성
+while True:
+    day += 1
+    visited = [[False] * c for _ in range(r)]
+    melted = bfs(0, 0)
+    melt(melted)
+    visited = [[False] * c for _ in range(r)]
+    swanL = swan(0, 0)
+    if len(swanL) == 2:
+        break
+print(day)
