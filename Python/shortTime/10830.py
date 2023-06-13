39cad625c9b49782853431451c9277d478537f4a
drowsy2762 <drowsy2762@gmail.com>
2023-06-13 15:21:41 +0900

    temp

--- Python/shortTime/10830.py
+++ Python/shortTime/10830.py
@@ -1,30 +1,14 @@
 # https://www.acmicpc.net/problem/10830 : 행렬제곱 (python3)
-import sys
-import copy
+from sys import stdin
 
-input = sys.stdin.readline
+input = stdin.readline
 
 
-def matmul(a, b, m, t):
-    sum = 0
-    for i in range(len(m)):
-        sum += t[a][i] * m[i][b]
-    return sum
+def divide():
+    f
 
 
 n, b = map(int, input().split())
-m = []
+a = []
 for i in range(n):
-    m.append(list(map(int, input().split())))
-r = copy.deepcopy(m)
-for _ in range(b - 1):
-    t = copy.deepcopy(r)
-    for i in range(n):
-        for j in range(n):
-            r[i][j] = matmul(i, j, m, t)
-for i in range(n):
-    for j in range(n):
-        while r[i][j] > 1000:
-            r[i][j] %= 1000
-        print(r[i][j], end=" ")
-    print()
+    a[i].append(list(map(int, input().split())))
