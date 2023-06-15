fa190b7818228916f4e57f3dad2663a363818504
drowsy2762 <drowsy2762@gmail.com>
2023-06-15 17:11:03 +0900

    10830 timeeror와 testcase answer solution

--- Python/shortTime/10830.py
+++ Python/shortTime/10830.py
@@ -1,20 +1,19 @@
 # https://www.acmicpc.net/problem/10830 : 행렬제곱 (python3)
-import sys
-
-input = sys.stdin.readline
 
 
-# 제곱을 분할해서 하는 함수
+# 제곱을 분할해서 하는 함수(제곱을 분할해 연산 과정을 줄임)
 def divide(x, y):
     if y == 1:
         return x
     elif y % 2 == 0:
-        return matmul(divide(x, y // 2), divide(x, y // 2))
+        temp = divide(x, y // 2)
+        return matmul(temp, temp)
     else:
-        return matmul(divide(x, y // 2), divide(x, y // 2 + 1))
+        temp = divide(x, y // 2)
+        return matmul(matmul(temp, temp), x)
 
 
-# 배열곱 함수
+# 행열곱 함수 (x와 y 행렬을 곱함)
 def matmul(x, y):
     size = len(x)
     r = [[0] * size for _ in range(size)]
@@ -34,9 +33,11 @@ matrix = []
 for i in range(n):
     matrix.append(list(map(int, input().split())))
 
+# 행렬 제곱 연산
 matrix = divide(matrix, square)
 
+# 출력 (마지막에 1000으로 나머지를 구하는 이유는 1000이하의 자연수이기 때문)
 for i in range(n):
     for j in range(n):
-        print(matrix[i][j], end=" ")
+        print(matrix[i][j] % 1000, end=" ")
     print()
