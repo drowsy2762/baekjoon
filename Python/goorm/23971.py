f5fd57438349335c39ba58de7cf5e03bad74075a
drowsy2762 <drowsy2762@gmail.com>
2023-09-11 19:28:18 +0900

    시간초과없이 clear

--- Python/goorm/23971.py
+++ Python/goorm/23971.py
@@ -1,17 +1,23 @@
 # https://www.acmicpc.net/problem/23971 : ZOAC 4 (python3)
 # input 최적화 작업
 from sys import stdin
+from math import ceil
 
 input = stdin.readline
 
+# 시간초과로 인한 실패로 방식을 바꿈
+# 1번 트리 참고 시간초과발생
+# 시간초과가 발생하는 이유는 잘 모르겠음
+# 방식을 바꿔서 직접적으로 풀기로함
+
 # 1이 몇개들어가는지 세기
-cnt = 0
+
 
 h, w, n, m = map(int, input().split())
 
-for i in range(0, h, n + 1):
-    for j in range(0, w, m + 1):
-        cnt += 1
+# 라인에 간격이 있을때 좌우로 얼마나 뛰어 앉았는지 계산
+rows = ceil(h / (n + 1))
+cols = ceil(w / (m + 1))
 
-
-print(cnt)
+# 두 라인을 곱해 최대 학생수를 구함
+print(rows * cols)
