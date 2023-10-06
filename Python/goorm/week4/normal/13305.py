299837147a049fd3ec4539770662af1e33253221
drowsy2762 <drowsy2762@gmail.com>
2023-10-06 18:42:20 +0900

    week4 clear

--- Python/goorm/week4/normal/13305.py
+++ Python/goorm/week4/normal/13305.py
@@ -6,4 +6,10 @@ n = int(input())
 length = list(map(int, input().split()))
 station = list(map(int, input().split()))
 
-for _ in range()
+min_price = station[0]
+total = 0
+for i in range(n - 1):
+    min_price = min(min_price, station[i])
+    total += min_price * length[i]
+
+print(total)
