3745360d569b334474976144053f31eb4bd06e4c
drowsy2762 <drowsy2762@gmail.com>
2023-11-02 20:59:02 +0900

    일단 완성

--- Python/goorm/week6/normal/19637.py
+++ Python/goorm/week6/normal/19637.py
@@ -1,27 +1,29 @@
 # https://www.acmicpc.net/problem/19637 : IF문 좀 대신 써줘
 # 2023-11-02
-from sys import stdin
+import sys
 
-input = stdin.readline
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
 
 def binarySearch(index):
-    if title[index - 1][1] <= cp < title[index][1]:
+    if index > n:
+        index = n
+    if title[index - 1][1] < cp <= title[index][1]:
         return index
-    elif title[index][1] > cp:
-        binarySearch(int(index / 2))
+    elif title[index][1] < cp:
+        return binarySearch(index + (index // 2) + 1)
     else:
-        binarySearch(index + int(index / 2))
+        return binarySearch(index // 2)
 
 
-title = [["0", 0]]
-# 10000 under WEAK, 100000 under NORMAL
-# 1000000 under STRONG
+title = [[0, 0]]
 n, m = map(int, input().split())
 for i in range(n):
     title.append(input().split())
-    title[i][1] = int(title[i][1])
+    title[i + 1][1] = int(title[i + 1][1])
 for _ in range(m):
     cp = int(input())
-    i = int(n / 2)
-    print(binarySearch(i))
+    j = int((n + 1) / 2)
+    tmp = binarySearch(i)
+    print(title[tmp][0])
