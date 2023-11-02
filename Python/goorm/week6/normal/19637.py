af9c3622216882122dea8497f8c352deb74931b0
drowsy2762 <drowsy2762@gmail.com>
2023-11-02 15:36:37 +0900

    version1

--- Python/goorm/week6/normal/19637.py
+++ Python/goorm/week6/normal/19637.py
@@ -4,13 +4,24 @@ from sys import stdin
 
 input = stdin.readline
 
-title = []
-cp = []
+
+def binarySearch(index):
+    if title[index - 1][1] <= cp < title[index][1]:
+        return index
+    elif title[index][1] > cp:
+        binarySearch(int(index / 2))
+    else:
+        binarySearch(index + int(index / 2))
+
+
+title = [["0", 0]]
 # 10000 under WEAK, 100000 under NORMAL
 # 1000000 under STRONG
 n, m = map(int, input().split())
 for i in range(n):
     title.append(input().split())
     title[i][1] = int(title[i][1])
-for i in range(m):
-    cp.append(int(input()))
+for _ in range(m):
+    cp = int(input())
+    i = int(n / 2)
+    print(binarySearch(i))
