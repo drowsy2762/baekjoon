bf5ecb92a50e0c495e6fff276d8b36c8528941d8
drowsy2762 <drowsy2762@gmail.com>
2023-11-07 12:22:13 +0900

    11501 clear

--- Python/goorm/week7/11501.py
+++ Python/goorm/week7/11501.py
@@ -4,40 +4,16 @@ from sys import stdin
 
 input = stdin.readline
 
-
-def getMax(start, end):
-    max, day = 0, 0
-    for i in range(start, end):
-        if days[i] > max:
-            max = days[i]
-            day = i
-    if day == 0:
-        return start
-    else:
-        return day
-
-
-def sell(start, end):
-    money = 0
-    for i in range(start, end):
-        money += days[end] - days[i]
-    return money
-
-
 T = int(input())
 for _ in range(T):
     N = int(input())
     days = list(map(int, input().split()))
     sequence = 0
-    start, money = 0, 0
+    max, money = 0, 0
 
-    while sequence != N - 1:
-        rtmp = sequence
-        sequence = getMax(sequence, N)
-        if rtmp == sequence:
-            sequence += 1
-            start = sequence
-            continue
-        money += sell(start, sequence)
-        start = sequence
+    for i in range(N - 1, -1, -1):
+        if days[i] > max:
+            max = days[i]
+        else:
+            money += max - days[i]
     print(money)
---
+++ Python/goorm/week7/1406.py
@@ -0,0 +1,2 @@
+# https://www.acmicpc.net/problem/1406 : 에디터 (python3)
+# 2023-11-09
