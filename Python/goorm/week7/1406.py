e7844549aab48c9ab6107c652562ea71eb68ce20
drowsy2762 <drowsy2762@gmail.com>
2023-11-08 22:52:04 +0900

    second

--- Python/goorm/week7/1406.py
+++ Python/goorm/week7/1406.py
@@ -2,45 +2,33 @@
 # 2023-11-09
 from sys import stdin
 
+# from collections import deque
+
 input = stdin.readline
 
-
-def moveCursor(cursor, command):
-    if command == "L":
-        if cursor == 0:
-            return cursor
-        else:
-            return cursor - 1
-    elif command == "D":
-        if cursor == len(string):
-            return cursor
-        else:
-            return cursor + 1
-    elif command == "B":
-        if cursor == 0:
-            return cursor
-        else:
-            return cursor - 1
-    elif command == "P":
-        return cursor
-
-
-string = input()
+string = input().rstrip()
+string = list(string)
 n = int(input())
 cursor = len(string)
 for i in range(n):
-    command = input()
+    command = input().rstrip()
     command = command.split()
     if command[0] == "P":
-        chrar = command[1]
+        char = command[1]
         command = command[0]
     else:
         command = command[0]
-    cursor = moveCursor(cursor, command)
-    if command == "B":
-        string = string[:cursor] + string[cursor + 1 :]
+    if command == "B" and cursor != 0:
+        del string[cursor]
+        cursor -= 1
+    elif command == "B" and cursor == 0:
+        del string[0]
     elif command == "P":
-        string = string[:cursor] + chrar + string[cursor:]
-
-
+        string.insert(cursor, char)
+        cursor += 1
+    elif command == "L" and cursor != 0:
+        cursor -= 1
+    elif command == "D" and cursor != len(string):
+        cursor += 1
+    print(string, cursor)
 print(string)
