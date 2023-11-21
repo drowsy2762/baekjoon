c9c136e527cc4ce59c48e6382b62ef8a93e34510
drowsy2762 <drowsy2762@gmail.com>
2023-11-21 12:04:24 +0900

    clear

--- Python/goorm/week9/17615.py
+++ Python/goorm/week9/17615.py
@@ -5,72 +5,20 @@ from collections import deque
 
 input = stdin.readline
 
+N = int(input())
+balls = str(input().rstrip())
+cnt = []
 
-def moving_R(s):
-    # print(s, "R")
-    cnt = 0
-    while True:
-        if s[0] != "R":
-            s.append("B")
-            s.popleft()
-            # print(cnt, "t")
-            cnt += 1
-            # print(cnt, "t1")
-        else:
-            break
-    i = 0
-    status = 0
-    lens = len(s)
-    while True:
-        if lens - 1 == i:
-            break
-        if s[i] != s[i + 1]:
-            status += 1
-        if status == 2:
-            s.append(s[i])
-            del s[i]
-            cnt += 1
-            status = 0
-            i = 0
-        i += 1
-    # print(s)
-    return cnt
+rexplore = balls.rstrip("R")
+cnt.append(rexplore.count("R"))
 
+rexplore = balls.rstrip("B")
+cnt.append(rexplore.count("B"))
 
-def moving_B(s):
-    cnt = 0
-    while True:
-        if s[0] != "B":
-            s.append("R")
-            s.popleft()
-            # print(cnt, "t")
-            cnt += 1
-            # print(cnt, "t1")
-        else:
-            break
-    i = 0
-    status = 0
-    lens = len(s)
-    while True:
-        if lens - 1 == i:
-            break
-        if s[i] != s[i + 1]:
-            status += 1
-        if status == 2:
-            s.append(s[i])
-            del s[i]
-            cnt += 1
-            status = 0
-            i = 0
-        i += 1
-    # print(s)
-    return cnt
+rexplore = balls.lstrip("R")
+cnt.append(rexplore.count("R"))
 
+rexplore = balls.lstrip("B")
+cnt.append(rexplore.count("B"))
 
-n = int(input())
-a = input().rstrip()
-b = deque(a[:])
-b.reverse()
-a = deque(a)
-# print(a, b)
-print(min(moving_R(b), moving_B(a), moving_R(a), moving_B(b)))
+print(min(cnt))
