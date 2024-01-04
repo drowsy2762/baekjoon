bab497566f6a6fcbb739417fa53b4a88765f7276
drowsy2762 <drowsy2762@gmail.com>
2024-01-05 05:18:20 +0900

    1문제 까먹고있었음

--- Python/goorm/week10/13549.py
+++ Python/goorm/week10/13549.py
@@ -1,5 +1,32 @@
 # https://www.acmicpc.net/problem/13549 : 숨바꼭질 3 (python3)
 # 2023-12-29
+from collections import deque
 from sys import stdin
 
 input = stdin.readline
+
+
+def bfs(n, m):
+    queue = deque()
+    queue.append(n)
+    while queue:
+        location = queue.popleft()
+        if location == m:
+            print(visited[location])
+            break
+        if 0 <= location * 2 < 100001 and visited[location * 2] == -1:
+            visited[location * 2] = visited[location]
+            queue.appendleft(location * 2)
+        if 0 <= location - 1 < 100001 and visited[location - 1] == -1:
+            visited[location - 1] = visited[location] + 1
+            queue.append(location - 1)
+        if 0 <= location + 1 < 100001 and visited[location + 1] == -1:
+            visited[location + 1] = visited[location] + 1
+            queue.append(location + 1)
+
+
+n, m = map(int, input().split())
+visited = [-1 for _ in range(100001)]
+visited[n] = 0
+
+bfs(n, m)
