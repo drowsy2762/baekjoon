5711e184a87bf420bcdf76df83c80c81a6bb6c42
drowsy2762 <drowsy2762@gmail.com>
2024-02-14 22:36:36 +0900

    1일 1문제

---
+++ Python/goorm/week14/2110.py
@@ -0,0 +1,40 @@
+# https://www.acmicpc.net/problem/2110 : 공유기 설치 (python3)
+# 2024-02-14
+from sys import stdin
+
+input = stdin.readline
+
+n, c = map(int, input().split())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+array.sort()
+
+
+def binary_search(array, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        current = array[0]
+        count = 1
+
+        for i in range(1, len(array)):
+            if array[i] >= current + mid:
+                count += 1
+                current = array[i]
+
+        if count >= c:
+            global answer
+            start = mid + 1
+            answer = mid
+        else:
+            end = mid - 1
+
+
+start = 1
+end = array[-1] - array[0]
+answer = 0
+
+binary_search(array, start, end)
+print(answer)
