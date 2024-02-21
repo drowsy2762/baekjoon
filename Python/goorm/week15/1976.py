b48386a1e79943d1d5128735642886583ac182d8
drowsy2762 <drowsy2762@gmail.com>
2024-02-21 23:14:55 +0900

    1일 1문제

--- Python/goorm/week15/1976.py
+++ Python/goorm/week15/1976.py
@@ -1,5 +1,48 @@
 # https://www.acmicpc.net/problem/1976 : 여행 가자 (Python3)
 # 2024-02-20
 from sys import stdin
+from collections import deque
 
 input = stdin.readline
+
+n = int(input())
+m = int(input())
+
+city = []
+
+for i in range(n):
+    city.append(list(map(int, input().split())))
+plan = list(map(int, input().split()))
+
+
+def bfs(s, e):
+    q = deque()
+    q.append(s)
+    visited = [False] * n
+    visited[s] = True
+
+    while q:
+        here = q.popleft()
+
+        if here == e:
+            return True
+
+        for i in range(n):
+            if city[here][i] == 1 and not visited[i]:
+                visited[i] = True
+                q.append(i)
+
+    return False
+
+
+answer = True
+for i in range(m - 1):
+    if plan[i] != plan[i + 1]:
+        if not bfs(plan[i] - 1, plan[i + 1] - 1):
+            answer = False
+            break
+
+if answer:
+    print("YES")
+else:
+    print("NO")
