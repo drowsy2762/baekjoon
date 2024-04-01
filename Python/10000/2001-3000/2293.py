788396f5ca708892cca3ede0f14dd4ddffdbe315
drowsy2762 <drowsy2762@gmail.com>
2024-04-01 22:01:29 +0900

    문제설명 주석 업데이트

--- Python/10000/2001-3000/2293.py
+++ Python/10000/2001-3000/2293.py
@@ -1,25 +1,33 @@
+from sys import stdin
+
 # https://www.acmicpc.net/problem/2293 : 동전 1 (python3)
 # 2024-03-31
-from sys import stdin
 
 input = stdin.readline
 
 
+# dp 함수는 동전들의 배열과 목표 금액을 인자로 받아,
+# 목표 금액을 만들 수 있는 동전의 조합의 수를 출력합니다.
 def dp(coins, k):
+    # dp 배열을 초기화합니다. dp[i]는 금액 i를 만들 수 있는 동전의 조합의 수를 저장합니다.
     dp = [0 for _ in range(k + 1)]
-    dp[0] = 1
-    for coin in coins:
-        for i in range(coin, k + 1):
+    dp[0] = 1  # 금액 0을 만드는 방법은 동전을 사용하지 않는 한 가지 방법이 있습니다.
+    for coin in coins:  # 각 동전에 대해
+        for i in range(coin, k + 1):  # 동전의 값부터 목표 금액까지
+            # 현재 금액(i)에서 동전의 값을 뺀 금액(i - coin)을 만드는 조합의 수를 더합니다.
             dp[i] += dp[i - coin]
-    print(dp[k])
+    print(dp[k])  # 목표 금액을 만들 수 있는 동전의 조합의 수를 출력합니다.
 
 
+# solution 함수는 문제의 입력을 받아 dp 함수를 호출합니다.
 def solution():
-    n, k = map(int, input().split())
-    coins = []
-    for _ in range(n):
-        coins.append(int(input()))
-    dp(coins, k)
+    n, k = map(int, input().split())  # 동전의 종류의 수와 목표 금액을 입력받습니다.
+    coins = []  # 동전들의 값을 저장할 배열을 초기화합니다.
+    for _ in range(n):  # 각 동전에 대해
+        coins.append(int(input()))  # 동전의 값을 입력받아 배열에 추가합니다.
+    dp(
+        coins, k
+    )  # dp 함수를 호출하여 목표 금액을 만들 수 있는 동전의 조합의 수를 출력합니다.
 
 
-solution()
+solution()  # solution 함수를 호출하여 프로그램을 시작합니다.
--- Python/20000/16001-17000/16928.py
+++ Python/20000/16001-17000/16928.py
@@ -3,44 +3,58 @@
 from sys import stdin
 from collections import deque
 
+input = stdin.readline
 
+
+# bfs 함수는 사다리와 뱀의 정보를 받아, 시작점에서 끝점까지 도달하는 데 필요한 최소 횟수를 출력합니다.
 def bfs(ladder, snake):
-    visited = [False for _ in range(101)]
-    graph = [0 for _ in range(101)]
-    q = deque([1])
-    while q:
-        t = q.popleft()
+    visited = [
+        False for _ in range(101)
+    ]  # 각 위치를 방문했는지 여부를 저장하는 배열입니다.
+    graph = [
+        0 for _ in range(101)
+    ]  # 각 위치에 도달하는 데 필요한 최소 횟수를 저장하는 배열입니다.
+    q = deque([1])  # BFS를 위한 큐입니다. 시작점은 1입니다.
+    while q:  # 큐가 빌 때까지 반복합니다.
+        t = q.popleft()  # 큐에서 가장 먼저 들어온 위치를 꺼냅니다.
 
-        if t == 100:
-            print(graph[t])
-            break
-        for dice in range(1, 7):
-            x = t + dice
-            if x <= 100 and not visited[x]:
-                if x in ladder.keys():
-                    x = ladder[x]
-                if x in snake.keys():
-                    x = snake[x]
-                if not visited[x]:
-                    visited[x] = True
-                    graph[x] = graph[t] + 1
-                    q.append(x)
+        if t == 100:  # 만약 끝점에 도달했다면
+            print(graph[t])  # 해당 위치에 도달하는 데 필요한 최소 횟수를 출력하고
+            break  # 반복을 종료합니다.
+        for dice in range(1, 7):  # 주사위의 모든 가능한 눈금에 대해
+            x = t + dice  # 주사위를 던져 이동한 위치를 계산합니다.
+            if (
+                x <= 100 and not visited[x]
+            ):  # 만약 이동한 위치가 보드 내에 있고 아직 방문하지 않았다면
+                if x in ladder.keys():  # 만약 이동한 위치에 사다리의 시작점이 있다면
+                    x = ladder[x]  # 사다리의 끝점으로 이동합니다.
+                if x in snake.keys():  # 만약 이동한 위치에 뱀의 머리가 있다면
+                    x = snake[x]  # 뱀의 꼬리로 이동합니다.
+                if not visited[x]:  # 만약 이동한 위치를 아직 방문하지 않았다면
+                    visited[x] = True  # 해당 위치를 방문했다고 표시하고
+                    graph[x] = (
+                        graph[t] + 1
+                    )  # 해당 위치에 도달하는 데 필요한 최소 횟수를 갱신하고
+                    q.append(x)  # 큐에 해당 위치를 추가합니다.
 
 
+# solution 함수는 문제의 입력을 받아 bfs 함수를 호출합니다.
 def solution():
     input = stdin.readline
 
-    n, m = map(int, input().split())
-    ladder = dict()
-    snake = dict()
-    for _ in range(n):
-        i, j = map(int, (input().split()))
-        ladder[i] = j
-    for _ in range(m):
-        i, j = map(int, (input().split()))
-        snake[i] = j
+    n, m = map(int, input().split())  # 사다리의 수와 뱀의 수를 입력받습니다.
+    ladder = dict()  # 사다리의 정보를 저장할 딕셔너리입니다.
+    snake = dict()  # 뱀의 정보를 저장할 딕셔너리입니다.
+    for _ in range(n):  # 각 사다리에 대해
+        i, j = map(int, (input().split()))  # 사다리의 시작점과 끝점을 입력받아
+        ladder[i] = j  # 딕셔너리에 저장합니다.
+    for _ in range(m):  # 각 뱀에 대해
+        i, j = map(int, (input().split()))  # 뱀의 머리와 꼬리를 입력받아
+        snake[i] = j  # 딕셔너리에 저장합니다.
 
-    bfs(ladder, snake)
+    bfs(
+        ladder, snake
+    )  # bfs 함수를 호출하여 시작점에서 끝점까지 도달하는 데 필요한 최소 횟수를 출력합니다.
 
 
-solution()
+solution()  # solution 함수를 호출하여 프로그램을 시작합니다.
