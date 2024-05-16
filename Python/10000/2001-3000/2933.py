85163420f72cd196250c0d4a5589152cb9d9c323
drowsy2762 <drowsy2762@gmail.com>
2024-05-16 11:48:07 +0900

    임시저장 못깼음

--- Python/10000/2001-3000/2933.py
+++ Python/10000/2001-3000/2933.py
@@ -15,26 +15,27 @@ r, c = map(int, input().split())
 cave = [list(input().rstrip()) for _ in range(r)]
 
 
+# 미네랄을 파괴하는 함수
 def destroy(y, direction):
     if direction == True:
         for i in range(c):
             if cave[y - 1][i] == "x":
                 cave[y - 1][i] = "."
                 direction = False
-                cluster = cluster(y - 1, i, cave)
+                cluster = cluster(y - 1, i)
     else:
         for i in range(0, c, -1):
             if cave[y - 1][i] == "x":
                 cave[y - 1][i] = "."
                 direction = True
-                cluster = cluster(y - 1, i, cave)
+                cluster = cluster(y - 1, i)
 
 
+# 떨어진 클러스터의 구성을 확인하는 함수
 def cluster(x, y):
     visited = [[0] * c for _ in range(r)]
     visited[x][y] = 1
     q = [(x, y)]
-    cluster = []
     while q:
         x, y = q.popleft()
         for i in range(4):
@@ -43,32 +44,18 @@ def cluster(x, y):
             if nx < 0 or nx >= r or ny < 0 or ny >= c:
                 continue
             if visited[nx][ny] == 0 and cave[nx][ny] == "x":
-                cluster.append((nx, ny))
                 visited[nx][ny] = 1
                 q.append((nx, ny))
-
-
-def gravity(cluster):
-    if not cluster:
-        return
-    while True:
-        for x, y in cluster:
-            if x == r - 1 or cave[x + 1][y] == "x":
-                return
-        for i in range(len(cluster)):
-            cluster[i] = (cluster[i][0] + 1, cluster[i][1])
+    return visited
 
 
 def solution():
     n = int(input())
     height = list(map(int, input().split()))
     direction = False
-    for i in height:
-        destroy(i, direction)
-        # cluster(cave)
 
     for i in range(r):
-        print("".join(cave))
+        print("".join(cave[i]))
 
 
 solution()
