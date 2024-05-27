eee0a69db959984aefcdf54f7aacf89ec0e412c2
drowsy2762 <drowsy2762@gmail.com>
2024-05-27 13:19:24 +0900

    Refactor code to improve readability and maintainability

--- Python/10000/2001-3000/2933.py
+++ Python/10000/2001-3000/2933.py
@@ -12,32 +12,28 @@ turn = True
 
 
 def destroy(y):
-    global turn
+    global turn, cave
     if turn == True:
         for i in range(c):
             if cave[r - y][i] == "x":
                 cave[r - y][i] = "."
                 turn = False
-                return (r - y, i)
     else:
         for i in range(c - 1, -1, -1):
             if cave[r - y][i] == "x":
                 cave[r - y][i] = "."
                 turn = True
-                return (r - y, i)
-    return None
 
 
-def findCluster(x, y):
+def find_cluster():
     visited = [[False] * c for _ in range(r)]
-    visited[x][y] = True
-    q = deque([(x, y)])
-    cluster = [(x, y)]
+    q = deque()
+    for i in range(c):
+        if visited[r - 1][i] == False and cave[r - 1][i] == "x":
+            visited[r - 1][i] = True
+            q.append((r - 1, i))
     while q:
-        if len(q) > 0:
-            x, y = q.popleft()
-        else:
-            break
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -49,49 +45,27 @@ def findCluster(x, y):
             ):
                 visited[nx][ny] = True
                 q.append((nx, ny))
-    return cluster
+    cluster = []
+    for i in range(r - 1, -1, -1):
+        for j in range(c):
+            if visited[i][j] == False and cave[i][j] == "x":
+                cluster.append((i, j))
+    if not cluster:
+        return cluster, 1, visited
+    else:
+        return cluster, 0, visited
 
 
-def isFloating(cluster):
+def drop_cluster(cluster, visited):
+    down_min = 1e9
     for x, y in cluster:
-        if x == r - 1 or (
-            x + 1 < r and cave[x + 1][y] == "x" and (x + 1, y) not in cluster
-        ):
-            return False
-    return True
-
-
-def applyGravity(cluster):
-    minFallDistance = r
-    for x, y in cluster:
-        fallDistance = 0
-        while (
-            x + fallDistance + 1 < r
-            and (x + fallDistance + 1, y) not in cluster
-            and cave[x + fallDistance + 1][y] == "."
-        ):
-            fallDistance += 1
-        minFallDistance = min(minFallDistance, fallDistance)
-
+        down = 0
+        while x + down + 1 < r and visited[x + down + 1][y] == False:
+            down += 1
+        down_min = min(down_min, down)
     for x, y in cluster:
         cave[x][y] = "."
-    for x, y in cluster:
-        cave[x + minFallDistance][y] = "x"
-
-
-def processTurn(height):
-    hit = destroy(height)
-    if hit:
-        floatingClusters = []
-        visited = [[False] * c for _ in range(r)]
-        for i in range(r):
-            for j in range(c):
-                if cave[i][j] == "x" and not visited[i][j]:
-                    cluster = findCluster(i, j)
-                    if isFloating(cluster):
-                        floatingClusters.append(cluster)
-        for cluster in floatingClusters:
-            applyGravity(cluster)
+        cave[x + down_min][y] = "x"
 
 
 # 메인 함수
@@ -102,11 +76,17 @@ def solution():
     n = int(input())
     heights = list(map(int, input().split()))
 
-    for height in heights:
-        processTurn(height)
-        for row in cave:
-            print("".join(row))
+    for i in range(n):
+        destroy(heights[i])
+        cluster, flag, visited = find_cluster()
+        if flag == 0:
+            drop_cluster(cluster, visited)
+        for i in range(r):
+            print("".join(cave[i]))
         print()
+
+    for i in range(r):
+        print("".join(cave[i]))
 
 
 solution()
