8ce496f96671104b46501ece0413794ebd30fda0
drowsy2762 <drowsy2762@gmail.com>
2024-05-28 14:11:53 +0900

    clear

--- Python/10000/2001-3000/2749.py
+++ Python/10000/2001-3000/2749.py
@@ -1,7 +1,21 @@
 # https://www.acmicpc.net/problem/2749 : 피본나치 수 3 (python3)
-# 2024-05-16
 from sys import stdin
 
 input = stdin.readline
 
-n = int(input())
+
+def solution():
+    mod = 1000000  # 10^6
+    fibo = [0, 1]
+    p = mod // 10 * 15  # 피사노 주기
+
+    n = int(input())
+
+    for i in range(2, p):
+        fibo.append(fibo[i - 1] + fibo[i - 2])
+        fibo[i] %= mod
+
+    print(fibo[n % p])
+
+
+solution()
