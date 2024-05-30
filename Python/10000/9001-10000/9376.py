08e9b5f9fa30b7c5f7492f4552ee98d3e8b66eec
drowsy2762 <drowsy2762@gmail.com>
2024-05-30 11:56:06 +0900

    죄수가 하나일때는 상관없는데 n명일때 문제가 발생함

--- Python/10000/9001-10000/9376.py
+++ Python/10000/9001-10000/9376.py
@@ -1,14 +1,53 @@
 # https://www.acmicpc.net/problem/9376 : 탈옥 (Python3)
-
 from sys import stdin
+from collections import deque
 
 input = stdin.readline
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs():
+    visited = [[False] * w for _ in range(h)]
+    money = [[0] * w for _ in range(h)]
+    q = deque()
+    for i in range(h):
+        for j in range(w):
+            if jail[i][j] == "$":
+                q.append((i, j, 0))
+                visited[i][j] = True
+
+    while q:
+        x, y, c = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and not visited[nx][ny]:
+                visited[nx][ny] = True
+                if jail[nx][ny] == "*":
+                    continue
+                if jail[nx][ny] == "#":
+                    q.append((nx, ny, c + 1))
+                    money[nx][ny] = c + 1
+                else:
+                    q.appendleft((nx, ny, c))
+                    money[nx][ny] = c
+
+    result = 1e9
+    for i in range(h):
+        for j in range(w):
+            if jail[i][j] == "#":
+                result = min(result, money[i][j])
+    for i in range(h):
+        print(money[i])
+    return result
 
 
 def jail_break():
-    global jail
+    global jail, w, h
     h, w = map(int, input().split())
     jail = [list(input().rstrip()) for _ in range(h)]
+    result = bfs()
+    print(result)
 
 
 def solution():
