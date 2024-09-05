0abe7a7d8c6c0867ac63906a55ae90e20353d336
drowsy2762 <drowsy2762@gmail.com>
2024-09-05 17:29:52 +0900

    clear

--- Python/10000/9001-10000/9376.py
+++ Python/10000/9001-10000/9376.py
@@ -1,59 +1,65 @@
 # https://www.acmicpc.net/problem/9376 : 탈옥 (Python3)
-from sys import stdin
+import sys
 from collections import deque
 
-input = stdin.readline
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+input = sys.stdin.readline
 
-
-def bfs():
-    visited = [[False] * w for _ in range(h)]
-    money = [[0] * w for _ in range(h)]
-    q = deque()
-    for i in range(h):
-        for j in range(w):
-            if jail[i][j] == "$":
-                q.append((i, j, 0))
-                visited[i][j] = True
-
-    while q:
-        x, y, c = q.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if 0 <= nx < h and 0 <= ny < w and not visited[nx][ny]:
-                visited[nx][ny] = True
-                if jail[nx][ny] == "*":
-                    continue
-                if jail[nx][ny] == "#":
-                    q.append((nx, ny, c + 1))
-                    money[nx][ny] = c + 1
-                else:
-                    q.appendleft((nx, ny, c))
-                    money[nx][ny] = c
-
-    result = 1e9
-    for i in range(h):
-        for j in range(w):
-            if jail[i][j] == "#":
-                result = min(result, money[i][j])
-    for i in range(h):
-        print(money[i])
-    return result
-
-
-def jail_break():
-    global jail, w, h
+testCase = int(input())
+for _ in range(testCase):
     h, w = map(int, input().split())
-    jail = [list(input().rstrip()) for _ in range(h)]
-    result = bfs()
+
+    graph = [["." for _ in range(w + 2)]]
+    for y in range(h):
+        row = list(input()[:-1])
+        graph.append(["."] + row + ["."])
+    graph.append(["." for _ in range(w + 2)])
+
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    def bfs(y, x):
+        dq = deque([(y, x)])
+        dist = [[-1 for _ in range(w + 2)] for _ in range(h + 2)]
+        dist[y][x] = 0
+        while dq:
+            y, x = dq.pop()
+            for d in range(4):
+                nx = x + dx[d]
+                ny = y + dy[d]
+                if (
+                    0 <= nx < w + 2
+                    and 0 <= ny < h + 2
+                    and graph[ny][nx] != "*"
+                    and dist[ny][nx] == -1
+                ):
+                    if graph[ny][nx] == "#":
+                        dist[ny][nx] = dist[y][x] + 1
+                        dq.appendleft((ny, nx))
+                    else:
+                        dist[ny][nx] = dist[y][x]
+                        dq.append((ny, nx))
+
+        return dist
+
+    # 수감자 위치 찾기
+    prisoner = []
+    for y, row in enumerate(graph):
+        for x, v in enumerate(row):
+            if v == "$":
+                prisoner.append((y, x))
+
+    dist1 = bfs(prisoner[0][0], prisoner[0][1])
+    dist2 = bfs(prisoner[1][0], prisoner[1][1])
+    dist3 = bfs(0, 0)
+
+    result = float("inf")
+    for y in range(h + 2):
+        for x in range(w + 2):
+            s = dist1[y][x] + dist2[y][x] + dist3[y][x]
+            if graph[y][x] == "#":
+                s -= 2
+
+            if s >= 0:
+                result = min(result, s)
+
     print(result)
-
-
-def solution():
-    n = int(input())
-    for _ in range(n):
-        jail_break()
-
-
-solution()
