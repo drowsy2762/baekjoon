d03f46eed6a6cff39d2ab715379c7f9eaa8eee42
drowsy2762 <drowsy2762@gmail.com>
2025-01-22 22:44:50 +0900

    test

--- Python/10000/2001-3000/2631.py
+++ Python/10000/2001-3000/2631.py
@@ -3,13 +3,28 @@ from sys import stdin
 
 input = stdin.readline
 
+# 다이나믹 프로그래밍을 통해 문제를 풀고싶음
+dp = []
+cnt = 0
+perfect = [i + 1 for i in range(n)]
+
+
+def dp(m):
+    weight = []
+    for i in range(len(m)):
+        weight.append(int(abs(i + 1 - m[i])))
+    move = weight.index(max(weight))
+
+    print(weight)
+
 
 def main():
     n = int(input())
     m = []
+    j = 0
     for i in range(n):
         m.append(int(input()))
-    print(m)
+    dp(m)
 
 
 main()
