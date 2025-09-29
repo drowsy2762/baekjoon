cf9aa11f932f15011eba4074f84c7618810eea1f
drowsy2762 <drowsy2762@gmail.com>
2025-09-29 19:02:03 +0900

    clear

--- Python/10000/1001-2000/1283.py
+++ Python/10000/1001-2000/1283.py
@@ -1,21 +1,30 @@
 # https://www.acmicpc.net/problem/1283 : 단축키 지정 (python3)
+# 2025-09-29
+from sys import stdin
+input = stdin.readline
 
 n = int(input())
-
-s = []
+strings = [input().rstrip().split() for _ in range(n)]
 skey = []
-for i in range(n):
-    s.append(input())
 
-for i in s:
-    temp = 0
-    cnt = 0
-    for j in i:
-        for k in s:
-            if j != k:
-                skey.append(j)
-                cnt += 1
-    if cnt == 1:
-        break
+for string in strings:
+    for i in range(len(string)):
+        if string[i][0].lower() not in skey:
+            skey.append(string[i][0].lower())
 
-print(skey)
+            string[i] = "[" + string[i][0] + "]" + string[i][1:]
+            break
+    else:
+        for i in range(len(string)):
+            found = False
+            for j in range(1, len(string[i])):
+                if string[i][j].lower() not in skey:
+                    skey.append(string[i][j].lower())
+                    found = True
+                    string[i] = string[i][:j] + '[' + string[i][j] + ']' + string[i][j+1:]
+                    break
+            if found:
+                break
+            
+for string in strings:
+    print(' '.join(string))
