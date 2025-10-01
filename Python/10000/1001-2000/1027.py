d31af5429946dd7671eae1b97fa41936f343818f
drowsy2762 <drowsy2762@gmail.com>
2025-10-01 22:45:47 +0900

    clear

--- Python/10000/1001-2000/1027.py
+++ Python/10000/1001-2000/1027.py
@@ -1,17 +1,22 @@
 # https://www.acmicpc.net/problem/1027 : 고층 건물 (python3)
 # 2024-02-21
-from sys import stdin
+# 2025-09-30 (retry)
+import sys
 
-input = stdin.readline
-
-
-def bruteforce(buildings):
-    while True:
-        i
+input = sys.stdin.readline
 
 
 def solution():
-    rear, front = 0
     n = int(input())
     buildings = list(map(int, input().split()))
     answer = [0] * n
+    for i in range(n - 1):
+        temp = -sys.maxsize
+        for j in range(i + 1, n):
+            if temp < (buildings[j] - buildings[i]) / (j - i):
+                answer[i] += 1
+                answer[j] += 1
+                temp = (buildings[j] - buildings[i]) / (j - i)
+    print(max(answer))
+
+solution()
