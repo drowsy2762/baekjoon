73a0c8c71ed47f6e55e343b743925e1713ece2b0
drowsy2762 <drowsy2762@gmail.com>
2025-10-22 00:17:29 +0900

    monotonic stack use

--- Python/10000/6001-7000/6549.py
+++ Python/10000/6001-7000/6549.py
@@ -1,17 +1,23 @@
 # https://www.acmicpc.net/problem/6549 : 히스토그램에서 가장 큰 직사각형 (python3)
+# 2025-10-14
 from sys import stdin
-
 input = stdin.readline
 
+
 while True:
-    n = list(map(int, input().split()))
-    arr = [0] * len(n) * 4
-    if n[0] == 0:
+    n, *heights = list(map(int, input().split()))
+    if(n == 0):
         break
-    max_extent = 0
-    for i in len(n) - 2:
-        h = 0
-
-
-# 구간합을 구해야할때 = 보다 낮아졌을떄
-# 그러면 좌 한번 우 한번 두번구해야함
+    
+    heights.insert(0, 0)
+    heights += [0]
+    checked = [0]
+    area = 0
+   
+    for i in range(1, n + 2):
+      
+        while(checked and (heights[checked[-1]] > heights[i])):
+            cur_height = checked.pop()
+            area = max(area, (i - 1 - checked[-1]) * heights[cur_height])
+        checked.append(i)
+    print(area)
