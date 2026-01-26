71c28777993e65a4253c41e1f21e6a05a311ad3f
drowsy2762 <drowsy2762@gmail.com>
2026-01-26 21:07:48 +0900

    clear

--- Python/10000/1001-2000/1759.py
+++ Python/10000/1001-2000/1759.py
@@ -1,11 +1,33 @@
-# https://www.acmicpc.net/problem/1759 : 암호 만들기 (python3)
-from sys import stdin
+# https://www.acmicpc.net/problem/1759
+# 2026-01-26
+import sys
+input = sys.stdin.readline
 
-input = stdin.readline
+L, C = map(int, input().split())
+chars = sorted(list(input().split()))
+vowels = ['a', 'e', 'i', 'o', 'u']
 
-l, c = map(int, input().split())
-alphabet = list(input().split())
+def check(password):
+    v_count = 0 # 모음 개수
+    c_count = 0 # 자음 개수
+    
+    for char in password:
+        if char in vowels:
+            v_count += 1
+        else:
+            c_count += 1
+        
+    if v_count >= 1 and c_count >= 2:
+        return True
+    return False
 
-alphabet.sort()
+def backtrack(start_idx, current_seq):
+    if len(current_seq) == L:
+        if check(current_seq):
+            print("".join(current_seq))
+        return
 
-print(alphabet)
+    for i in range(start_idx, C):
+        backtrack(i + 1, current_seq + [chars[i]])
+
+backtrack(0, [])
