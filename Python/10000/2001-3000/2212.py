6302c2d3ef31926a9be52e2da0ccd06ecdc5e7a6
drowsy2762 <drowsy2762@gmail.com>
2026-02-24 23:39:28 +0900

    clear

---
+++ Python/10000/2001-3000/2212.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/2212
+# 2026-02-24
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+pos = sorted(list(map(int, input().split())))
+
+if k >= n:
+    print(0)
+    sys.exit()
+
+dist = []
+for i in range(1, n):
+    dist.append(pos[i] - pos[i-1])
+
+dist.sort(reverse=True)
+for _ in range(k-1):
+    dist.pop(0)
+
+print(sum(dist))
