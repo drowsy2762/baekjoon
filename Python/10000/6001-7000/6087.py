62cd942884ea71485a1a0eba2f4c9cfc35d129d3
drowsy2762 <drowsy2762@gmail.com>
2026-03-08 14:53:33 +0900

    clear

--- Python/10000/6001-7000/6087.py
+++ Python/10000/6001-7000/6087.py
@@ -1,20 +1,43 @@
 # https://www.acmicpc.net/problem/6087 : 레이저 통신 (python3)
-# 2025-10-23
+# 2025-10-23(can't solve) / 2026-03-08(solve)
 from sys import stdin
 from collections import deque
 
 input = stdin.readline
 
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+dr, dc = [1, -1, 0, 0], [0, 0, 1, -1]
 
 w, h = map(int,input().split())
-graphs = [list(input().rstrip()) for _ in range(h)]
+graph = [list(input().rstrip()) for _ in range(h)]
+dist = [[float('inf')] * w for _ in range(h)]
 
-# 방향이 바뀔때마다 거울을 추가한다고 봐야함
-# 2안 벽 혹은 구조물에 접촉할때까지 일자로 직진함
-# 3안 방향을 관리함 visited 리스트를 3차원으로 설계한후 방향을 관리함 
-def bfs(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
- 
+c_pos = []
+for i in range(h):
+    for j in range(w):
+        if graph[i][j] == "C":
+            c_pos.append((i, j))
+(sr, sc), (er, ec) = c_pos[0], c_pos[1]
+
+def bfs(sr, sc):
+    q = deque()
+    q.append((sr, sc))
+    dist[sr][sc] = -1
+    while q:
+        r, c = q.popleft()
+        if r == er and c == ec:
+            return dist[er][ec]
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            while 0 <= nr < h and 0 <= nc < w and graph[nr][nc] != "*":
+                if dist[nr][nc] >= dist[r][c] + 1:
+                    if dist[nr][nc] > dist[r][c] + 1:
+                        dist[nr][nc] = dist[r][c] + 1
+                        q.append((nr, nc))
+                    nr += dr[i]
+                    nc += dc[i]
+                elif dist[nr][nc] < dist[r][c] + 1:
+                    break 
+
+
+
+print(bfs(sr, sc))
