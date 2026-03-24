b59b68445157e89526a583c49e6eb147507cc4e7
drowsy2762 <drowsy2762@gmail.com>
2026-03-24 15:50:49 +0900

    최적화

--- Python/10000/1001-2000/1956.py
+++ Python/10000/1001-2000/1956.py
@@ -6,22 +6,23 @@ INF = int(1e9)
 
 v, e = map(int, input().split())
 graph = [[INF] * (v + 1) for _ in range(v + 1)]
-for i in range(e):
+for _ in range(e):
     a, b, c = map(int, input().split())
     graph[a][b] = c
 
 for k in range(1, v + 1):
     for i in range(1, v + 1):
+        if graph[i][k] == INF:
+            continue
+        ik_dist = graph[i][k]
         for j in range(1, v + 1):
-            if graph[i][k] + graph[k][j] < graph[i][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
+            new_dist = ik_dist + graph[k][j]
+            if new_dist < graph[i][j]:
+                graph[i][j] = new_dist
 
 answer = INF
 for i in range(1, v + 1):
-    answer = min(answer, graph[i][i])
+    if graph[i][i] < answer:
+        answer = graph[i][i]
 
-if answer == INF:
-    print(-1)
-else:
-    print(answer)
-
+print(answer if answer != INF else -1)
