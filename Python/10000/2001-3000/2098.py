cb2c75c598009084c5dc77eedb8bb6d587d32f34
drowsy2762 <drowsy2762@gmail.com>
2026-03-26 22:02:35 +0900

    clear

---
+++ Python/10000/2001-3000/2098.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/2098
+# 2026-03-26
+import sys
+
+N = int(sys.stdin.readline())
+W = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+DP = [[-1] * N for _ in range(1 << N)]
+INF = 10**9
+
+def tsp(visited, curr):
+    if visited == (1 << N) - 1:
+        return W[curr][0] if W[curr][0] != 0 else INF
+    if DP[visited][curr] != -1:
+        return DP[visited][curr]
+    
+    min_cost = INF
+    for nxt in range(N):
+        if not (visited & (1 << nxt)) and W[curr][nxt] != 0:
+            cost = tsp(visited | (1 << nxt), nxt) + W[curr][nxt]
+            min_cost = min(min_cost, cost)
+    
+    DP[visited][curr] = min_cost
+    return min_cost
+
+print(tsp(1 << 0, 0))
