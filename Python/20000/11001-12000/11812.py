9837f5d0076ac9b17e706280f907620bdac9d7fd
drowsy2762 <drowsy2762@gmail.com>
2026-04-13 14:26:55 +0900

    Refactor solution function to use results list for output and remove LCA function

--- Python/20000/11001-12000/11812.py
+++ Python/20000/11001-12000/11812.py
@@ -4,24 +4,26 @@ import sys
 
 def solution():
     input = sys.stdin.readline
-
-    def LCA(x, y):
-        d = 0
+    
+    n, k, q = map(int, input().split())
+    results = []
+    
+    for _ in range(q):
+        x, y = map(int, input().split())
+        if k == 1:
+            results.append(str(abs(x - y)))
+            continue
+        
+        dist = 0
         while x != y:
             if x > y:
                 x = (x + k - 2) // k
             else:
                 y = (y + k - 2) // k
-            d += 1
-        return d
-
-    n, k, q = map(int, input().split())
-    for _ in range(q):
-        x, y = map(int, input().split())
-        if k == 1:
-            print(abs(x - y))
-            continue
-        print(LCA(x, y))
+            dist += 1
         
+        results.append(str(dist))
+    sys.stdout.write('\n'.join(results) + '\n')
+
 if __name__ == "__main__":
     solution()
